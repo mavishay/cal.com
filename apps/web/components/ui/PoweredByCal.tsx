af3a6330a7fc24dc64a47105e941045d8cda057c
Avishay Maor <avishaym@melio.com>
2022-12-09 12:18:44 +0200

    remove powerby

--- apps/web/components/ui/PoweredByCal.tsx
+++ apps/web/components/ui/PoweredByCal.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 
 import { useIsEmbed } from "@calcom/embed-core/embed-iframe";
-import { POWERED_BY_URL } from "@calcom/lib/constants";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
 
 const PoweredByCal = () => {
@@ -9,25 +8,9 @@ const PoweredByCal = () => {
   const isEmbed = useIsEmbed();
   return (
     <div className={"p-2 text-center text-xs sm:text-right" + (isEmbed ? " max-w-3xl" : "")}>
-      <Link href={POWERED_BY_URL}>
+      <Link href="https://www.videohaus.co">
         <a target="_blank" className="text-bookinglight opacity-50 hover:opacity-100 dark:text-white">
-          {t("powered_by")}{" "}
-          {
-            // eslint-disable-next-line @next/next/no-img-element
-            <img
-              className="relative -mt-px inline h-[10px] w-auto dark:hidden"
-              src="/cal-logo-word.svg"
-              alt="Cal.com Logo"
-            />
-          }
-          {
-            // eslint-disable-next-line @next/next/no-img-element
-            <img
-              className="relativ -mt-px hidden h-[10px] w-auto dark:inline"
-              src="/cal-logo-word-dark.svg"
-              alt="Cal.com Logo"
-            />
-          }
+          {t("powered_by")} Videohaus.co
         </a>
       </Link>
     </div>
